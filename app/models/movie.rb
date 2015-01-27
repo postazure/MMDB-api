26c93e6719389eb0a4583c9a169746f3666bf20b
Max Gordon <gordonmaxc@gmail.com>
2015-01-27 11:53:51 -0800

    regex validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ActiveRecord::Base
   validates :title, uniqueness: {scope: :year}
+  validates :image_url, format: {with: /\Ahttp:\/\/+.+/}
+  validates :year, format: {with: /[12]\d{3}/}
+  validates :plot, format: {with: /[12]\d{3}/}
 end
